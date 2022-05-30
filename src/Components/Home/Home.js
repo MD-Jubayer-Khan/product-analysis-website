5e91479738fb7213f1bb15b27317ba7239f218ac
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-05-30 13:46:46 +0600

    created reviews data

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,19 +4,24 @@ import './Home.css';
 
 const Home = () => {
     return (
-        <div className='container' style={{marginTop:'30px',width: '95%', margin:'auto'}}>
-          <div className='content' style={{}}>
+       <div>
+          <div className='container' style={{marginTop:'30px',width: '95%', margin:'auto'}}>
+          <div className='content' style={{marginTop:"50px"}}>
             <h3 style={{fontSize:'45px',color:'rgb(56, 53, 253)'}}>BD Electronics</h3>
             <p style={{color: 'gray', fontSize:'25px'}}>You can collect all kind of electronics product from here. such as TV, Laptop, mobile, Watch, I Mac</p>
-            <button style={{border:'none', backgroundColor:'rgb(161, 211, 255)', color:'blue', fontSize:"15px", height:'50px', width:'150px', borderRadius:'6px'}}>Live Demo</button>
+            <button style={{border:'none', backgroundColor:'rgb(214, 236, 255)', color:'blue', fontSize:"15px", height:'50px', width:'150px', borderRadius:'6px', marginTop:'50px', marginRight:'200px'}}>Live Demo</button>
           </div> 
           <div>
-            <img style={{height:"600px", width:'700px'}} src={image} alt="" />
-          </div>
-          <div>
-            <h1>Our customer review</h1>
+            <img style={{height:"600px", width:'700px', marginTop:'30px'}} src={image} alt="" />
           </div>
         </div>
+        <div>
+        <div style={{height:'280px', marginTop:"20px"}}>
+            <h1>Our customer review</h1>
+          </div>
+          <button style={{border:'none', backgroundColor:'rgb(161, 211, 255)', color:'blue', fontSize:"20px", height:'40px', width:'150px', borderRadius:'6px', marginTop:'50px', marginBottom:'20px'}}>See all reviews</button>
+        </div>
+       </div>
     );
 };
 
