56c0c5f26a01516aaf34f6243f9a39d2eb78d7f2
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-05-30 12:09:14 +0600

    create home component details

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import image from './image/multiple.png';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-          <h1>This is home page</h1>  
+        <div className='container' style={{marginTop:'30px',width: '95%', margin:'auto'}}>
+          <div className='content' style={{}}>
+            <h3 style={{fontSize:'45px',color:'rgb(56, 53, 253)'}}>BD Electronics</h3>
+            <p style={{color: 'gray', fontSize:'25px'}}>You can collect all kind of electronics product from here. such as TV, Laptop, mobile, Watch, I Mac</p>
+            <button style={{border:'none', backgroundColor:'rgb(161, 211, 255)', color:'blue', fontSize:"15px", height:'50px', width:'150px', borderRadius:'6px'}}>Live Demo</button>
+          </div> 
+          <div>
+            <img style={{height:"600px", width:'700px'}} src={image} alt="" />
+          </div>
+          <div>
+            <h1>Our customer review</h1>
+          </div>
         </div>
     );
 };
 
-export default Home;<h1>This is home page</h1>
+export default Home;
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,6 +4,7 @@ import image from './image/404-errors.png'
 const NotFound = () => {
     return (
         <div style={{marginTop :"40px"}}>
+            <h3>Page not found!!</h3>
             <img src={image} alt="" />
         </div>
     );
